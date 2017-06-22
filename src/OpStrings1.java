b262883ac2a9d780d17e0db748894c8dd4eab919
jlee512 <jlee512@aucklanduni.ac.nz>
2017-06-23 10:37:09 +1200

    Moves in squared strings (II) Kata

--- src/OpStrings1.java
+++ src/OpStrings1.java
@@ -1,5 +1,34 @@
+import java.util.concurrent.Callable;
+
 /**
  * Created by Julian on 23/06/2017.
  */
+
 public class OpStrings1 {
+
+    public static String rot(String strng) {
+        // your code
+
+        return "";
+    }
+
+    public static String selfieAndRot(String strng) {
+        // your code
+
+        return "";
+    }
+    public static String oper(Callable<String> method_selection, String s) {
+        // your code and complete ... before operator
+
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+        String test = "abcd\nefgh\nijkl";
+
+        System.out.println(test);
+
+    }
 }
