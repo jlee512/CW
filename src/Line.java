6b9799397fdb197bfc05649859e2ee37b4c1cb8d
jlee512 <jlee512@aucklanduni.ac.nz>
2017-06-28 08:52:41 +1200

    Double Cola Kata - initial commit

---
+++ src/Line.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Julian on 28/06/2017.
+ */
+
+
+public class Line {
+    public static String WhoIsNext(String[] names, int n)
+    {
+
+        //Establish the length of the queue
+        int num_names
+
+
+    }
+}
